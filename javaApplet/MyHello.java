0832144739b3c797efba2120501b7035a1d8bbb6
Kose-i <tamura.kosei905@mail.kyutech.jp>
2020-12-10 02:00:52 +0900

    Add javaApplet

---
+++ javaApplet/MyHello.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.applet.*;
+
+class MyHello extends Applet {
+  public void paint(Graphics g) {
+    g.drawString("This is a test", 0, 50);
+  }
+  //public static void main(String[] Args) {
+  //  System.out.println("temp");
+  //}
+}
